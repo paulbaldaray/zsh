c7950529f1748895e9575149215912014739144d
Paul Baldaray <paul.baldaray@snowflake.com>
2025-07-31 22:50:04 -0700

    cleanup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cmd='source ~/.config/zsh/zshrc' 
+cmd='source ~/.config/zsh/zshrc'
 rc="$HOME/.zshrc"
-found=`grep "$cmd" ~/.zshrc 2> /dev/null`
+found=$(grep "$cmd" ~/.zshrc 2>/dev/null)
 if [[ -z "$found" ]]; then
   echo "Adding '$cmd' to $rc"
-  echo "$cmd" >> "$rc"
+  echo "$cmd" >>"$rc"
 else
   echo "'$cmd' already present in $rc"
 fi
