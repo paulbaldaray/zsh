7cc47de001a1e5cd87b85d15dcfc7a4dfcedb49d
Paul Baldaray <paul.baldaray@snowflake.com>
2025-08-03 12:46:26 -0700

    setup colors

--- aliases.zsh
+++ aliases.zsh
@@ -1,2 +1,7 @@
-### Aliases ###
+### COLORS ###
 alias ls='ls --color=tty'
+if command -v gdircolors >/dev/null 2>&1; then
+    eval "$(gdircolors)"
+elif command -v dircolors >/dev/null 2>&1; then
+    eval "$(dircolors)"
+fi
